c31a122f0e71f739180bb4d22b3e242169c939eb
Wilfredo Velazquez-Rodriguez <will.velazquez@flexibits.com>
2024-03-14 09:23:43 -0400

    image test

---
+++ Pages/ImageTest.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace App1.Pages;
+
+internal sealed partial class ImageTest : Page
+{
+	public ImageTest()
+    {
+        this.InitializeComponent();
+    }
+}
